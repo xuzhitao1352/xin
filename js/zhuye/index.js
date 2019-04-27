29f40dc11607735cff921464478acf2d32ebdea4
xuzhitao1352 <xzt_it@163.com>
2019-04-27 08:38:32 +0800

    顶部视频项移动交互

--- js/zhuye/index.js
+++ js/zhuye/index.js
@@ -1,4 +1,38 @@
 var log = console.log;
-//推广上放视频项
-var slide_item = document.querySelectorAll(".swiper-slide .item");
-log(slide_item);
+/*第一排视频项*/
+var slide_item = document.querySelectorAll(".swiper-right-box .item");
+//鼠标交互事件
+for(var i = 0;i<slide_item.length;i++){
+    slide_item[i].onmouseenter = function(){
+        log(this);
+        var span = this.children[1];
+        var title = span.innerText;
+        span.style.display = "none";
+        //获取图片高度
+        var h =this.children[0].offsetHeight;
+        //获取图片top
+        var t = this.children[0].offsetTop;
+        //创建盒子(背景)
+        var b = document.createElement("div");
+        b.style.height = h+"px";
+        b.style.top = t+"px";
+        b.className = "active";
+        b.innerHTML = `
+            <div class="title">${title}</div>
+            <div>
+                <b>up主:??????</b>
+                <b>up主:13.6万</b>
+                <i></i>
+            </div>
+        `;
+        this.appendChild(b);
+        
+    };
+    slide_item[i].onmouseleave = function(){
+        var title = this.children[1];
+        title.style.display = "inline";
+        var b =this.children[2];
+        this.removeChild(b);
+    };
+}
+
